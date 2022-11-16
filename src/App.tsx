a948d5878768b992f728179ea81b1faecc605f22
KNIDZE <mihaknidze@gmail.com>
2022-11-16 10:44:32 +0200

    favic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Favicon from 'react-favicon';
 import Board from './pages/Board/Board';
 import Home from './pages/Home/Home';
 import { CardModal } from './pages/Board/components/Card/CardModal/CardModal';
@@ -8,8 +9,12 @@ import { Login } from './pages/Login/Login';
 import { Registration } from './pages/Registration/Registration';
 
 function App(): React.ReactElement {
+  useEffect(() => {
+    document.title = 'SimpleBoards';
+  });
   return (
     <div className="App">
+      <Favicon url="src/favic.ico" />
       <header className="App-header">
         <Router>
           <Routes>
