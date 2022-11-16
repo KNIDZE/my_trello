6c0f0e57bcbd07c15973481c9ba0e229e14787d0
KNIDZE <mihaknidze@gmail.com>
2022-11-16 10:54:16 +0200

    favic done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Favicon from 'react-favicon';
 import Board from './pages/Board/Board';
 import Home from './pages/Home/Home';
 import { CardModal } from './pages/Board/components/Card/CardModal/CardModal';
@@ -14,7 +13,6 @@ function App(): React.ReactElement {
   });
   return (
     <div className="App">
-      <Favicon url="src/favic.ico" />
       <header className="App-header">
         <Router>
           <Routes>
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
+import Favicon from 'react-favicon';
 import store from './store/store';
 import App from './App';
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Favicon url="https://raw.githubusercontent.com/KNIDZE/my_trello/main/src/favic.ico" />
       <App />
     </Provider>
   </React.StrictMode>
