b819dbc4045935536b3350f40d461f36a4cb5308
KNIDZE <mihaknidze@gmail.com>
2022-11-17 23:09:38 +0200

    home mobile done/ work with list

--- src/pages/Home/components/BoardCreator/BoardCreator.tsx
+++ src/pages/Home/components/BoardCreator/BoardCreator.tsx
@@ -3,19 +3,12 @@ import BoardCreatorModule from './BoardCreatorModul';
 
 export default function BoardCreator(): JSX.Element {
   const [boardCreatorVisible, changeCreatorsVisibility] = useState(false);
-  if (boardCreatorVisible) {
-    return (
-      <div>
-        <div onClick={(): void => changeCreatorsVisibility(false)} className="add_board board">
-          <b>+ Add board</b>
-        </div>
-        <BoardCreatorModule />
-      </div>
-    );
-  }
   return (
-    <div onClick={(): void => changeCreatorsVisibility(true)} className="add_board board">
-      <b>+ Add board</b>
+    <div className="board_creator_div">
+      <div onClick={(): void => changeCreatorsVisibility(!boardCreatorVisible)} className="add_board board">
+        <b>+ Add board</b>
+      </div>
+      {boardCreatorVisible && <BoardCreatorModule />}
     </div>
   );
 }
